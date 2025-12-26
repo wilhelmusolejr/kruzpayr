2e1b3711863b05a213cb633fca29eff31e4c7b7b
wilhelmusolejr <wilhelmus.olejr@gmail.com>
2025-12-26 12:25:27 +0800

    testing

--- automation/tools/playground.py
+++ automation/tools/playground.py
@@ -3,21 +3,38 @@ import pydirectinput
 import time 
 import json
 import sys
+import requests
+import logging
 
 time.sleep(2)
 
+RUNNER_ID = "VM1"
+DASHBOARD_API = "http://localhost:3001"
+
 print("Screen size at click time:", pyautogui.size())
 
-def takeScreenshot(fileName):
-    current_roi = pyautogui.screenshot()
-    path = "logs/images/"
-    current_roi.save(path + fileName + ".png")
+def send_log(level, message):
+    try:
+        requests.post(
+            f"{DASHBOARD_API}/log",
+            json={
+                "level": level,
+                "message": message,
+                "runner": RUNNER_ID
+            },
+            timeout=3
+        )
+    except Exception:
+        pass  # never crash bot because of dashboard
 
-
-takeScreenshot("test")
-
+send_log("INFO", "Login started")
+send_log("ERROR", "Login failed")
 
 # --------------------
 sys.exit()
 # --------------------
+
+
+
+
 
